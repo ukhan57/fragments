e8f39e34606cdd57adc5545fe5907ee0325fa8a1
ypark91 <ypark91@myseneca.ca>
2024-01-29 08:38:37 -0500

    Add unit test usign jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,17 @@
+// .eslintrc.js
+
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {},
+};
