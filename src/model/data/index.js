46f87fd7f7ab7650c9ac874f1e0a1d9dfdc7f824
ypark91 <ypark91@myseneca.ca>
2024-02-07 19:49:08 -0500

    Add back-end data strategy

---
+++ src/model/data/index.js
@@ -0,0 +1,4 @@
+// src/model/data/index.js
+
+// To pick the appropriate back-end data strategy (we currently only have 1, our memory strategy, but we'll add ones for AWS later)
+module.exports = require('./memory');
