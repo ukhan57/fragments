abaa0bdcac9be3c1a45fe5704fde3b4c5f1a9a6c
ypark91 <ypark91@myseneca.ca>
2024-02-07 19:32:24 -0500

    Add fragment database using in-memory database and tests

---
+++ tests/unit/memory.test.js
@@ -0,0 +1,55 @@
+// ./tests/unit/memory.test.js
+
+// Fix this path to point to your project's `memory-db.js` source file
+const MemoryDB = require('../../src/model/data/memory/index');
+
+describe('Fragment Databse Tests', () => {
+
+  // writeFragments
+  test('write fragments meta data', async () => {
+    const fragment = {
+      ownerId: 'a',
+      id: 'b',
+      data: 'sampleData'
+    }
+    const result = await MemoryDB.writeFragment(fragment);
+    expect(result).toEqual(undefined);
+  })
+
+  // readFragments
+  test('read fragments meta data', async () => {
+    const fragment = {
+      ownerId: 'a',
+      id: 'b',
+      data: 'sampleData'
+    };
+    await MemoryDB.writeFragment(fragment);
+    const result = await MemoryDB.readFragment(fragment.ownerId, fragment.id);
+    expect(result).toEqual(fragment);
+  })
+
+  // writeFragmentData
+  test('write fragment data', async () => {
+    const fragment = {
+      ownerId: 'a',
+      id: 'b',
+      data: 'sampleData'
+    };
+    const buffer = Buffer.from(fragment.data);
+    const result = await MemoryDB.writeFragmentData(fragment.ownerId, fragment.id, buffer);
+    expect(result).toEqual(undefined);
+  })
+
+  // readFragmentData
+  test('write fragment data', async () => {
+    const fragment = {
+      ownerId: 'a',
+      id: 'b',
+      data: 'sampleData'
+    };
+    const buffer = Buffer.from(fragment.data);
+    await MemoryDB.writeFragmentData(fragment.ownerId, fragment.id, buffer);
+    const result = await MemoryDB.readFragmentData(fragment.ownerId, fragment.id);
+    expect(result).toEqual(buffer);
+  })
+});
