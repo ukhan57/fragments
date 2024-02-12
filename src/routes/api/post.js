da49d1a81e0eaac09f7fe7c15e7d3b591478d75e
ukhan57 <ukhan57@myseneca.ca>
2024-02-12 18:02:13 -0500

    add POST route and test

---
+++ src/routes/api/post.js
@@ -0,0 +1,41 @@
+// src/routes/api/post.js
+
+const { Fragment } = require('../../model/fragment');
+require('dotenv').config();
+
+const { createSuccessResponse, createErrorResponse } = require ('../../response');
+const logger = require('../../logger');
+
+/*
+ * Create a fragment for the current user
+*/
+module.exports = async (req, res) => {
+  try {
+    if (!Buffer.isBuffer(req.body)) {
+      return res.status(415).json(createErrorResponse(415, 'Unsupported Content-Type'));
+    }
+
+    // const type = req.get('Content-Type');
+    
+    // To support text/plain type of fragments only
+    if (!Fragment.isSupportedType(req.get('Content-Type'))) {
+      return res.status(415).json(createErrorResponse(415, 'Unsupported Content-Type'));
+    }
+
+    const ownerId = req.user;
+    const fragment = new Fragment({ownerId, type: req.get('Content-Type'), size: req.body.length,});
+
+    await fragment.save();
+    await fragment.setData(req.body);
+    
+    res.setHeader('Content-Type', fragment.type);
+    res.setHeader('Location', `${process.env.API_URL}/v1/fragments/${fragment.id}`);
+
+    res.status(201).json(createSuccessResponse({fragment: fragment}));
+
+    logger.info({ fragment: fragment }, `Fragment successfully posted`);
+  } catch (error) {
+    logger.Error({error}, 'Error creating fragment');
+    res.status(500).json(createErrorResponse(500, `${error}`));
+  }
+};
---
+++ tests/unit/post.test.js
@@ -0,0 +1,39 @@
+// tests/unit/get.test.js
+const request = require('supertest');
+const app = require('../../src/app');
+// const logger = require('../../src/logger')
+
+require('dotenv').config();
+
+describe('POST /v1/fragments', () => {
+  // // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => request(app).post('/v1/fragments').expect(401));
+
+  // // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+    test('fragment without data does not work', async () => {
+      const res = await request(app).post('/v1/fragments').auth('user1@email.com', 'password1').send();
+      expect(res.statusCode).toBe(500);
+    });
+
+  test('POST response includes a Location header with a full URL to GET the created fragment', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is fragment');
+    expect(res.statusCode).toBe(201);
+    expect(res.headers.location).toEqual(`${process.env.API_URL}/v1/fragments/${JSON.parse(res.text).fragment.id}`);
+  });
+
+  test('Fragment with an unsupported type gives error', () => 
+    request(app)
+      .post('/v1/fragments')
+      .set('Content-Type', 'audio/mpeg')
+      .auth('user1@email.com', 'password1')
+      .send('aa')
+      .expect(415)
+  );
+});
