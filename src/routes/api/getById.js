0a165d1249d2b758e6c411761d1fcd9e7fd77b44
ukhan57 <ukhan57@myseneca.ca>
2024-02-13 13:45:09 -0500

    Fixing bugs for getById

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -5,14 +5,14 @@ const logger = require('../../logger');
 const { createErrorResponse, createSuccessResponse } = require ('../../response');
 
 module.exports = async (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
   try {
     // check if fragment exists
     const fragment = await Fragment.byId(req.user, id);
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Content-Type', 'text/plain');
     res.status(200).json(createSuccessResponse(fragment));
   } catch (err) {
-    logger.warn(err);
+    logger.warn(err, "Error retrieving fragment(s)");
     res.status(404).json(createErrorResponse(404, 'Fragment not found'));
   }
 };
--- tests/unit/getById.test.js
+++ tests/unit/getById.test.js
@@ -2,9 +2,10 @@
 
 const request = require('supertest');
 const app = require('../../src/app');
+const logger = require('../../src/logger');
 
 describe('GET /v1/fragments/:id', () => {
-  // // If the request is missing the Authorization header, it should be forbidden
+  // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () =>
     request(app).get('/v1/fragments/111').expect(401));
 
@@ -20,54 +21,22 @@ describe('GET /v1/fragments/:id', () => {
     expect(res.body.error.message).toBe('Fragment not found');
   });
 
-  // test('authenticated users get a fragment', async () => {
-  //   const postRes = await request(app)
-  //   .post('/v1/fragments')
-  //   .auth('user1@email.com', 'password1')
-  //   .set('Content-Type', 'text/plain')
-  //   .send('This is a fragment');
+  test('authenticated users get a fragment', async () => {
+    const postRes = await request(app)
+    .post('/v1/fragments')
+    .auth('user1@email.com', 'password1')
+    .set('Content-Type', 'text/plain')
+    .send('This is a fragment');
 
-  //   // Check if the fragment is successfully created
-  //   expect(postRes.statusCode).toBe(201);
+    expect(postRes.statusCode).toBe(201);
 
-  //   // Extract the ID of the created fragment from the response
-  //   let fragmentId;
-  //   if ('fragment' in postRes.body && 'id' in postRes.body.fragment) {
-  //     fragmentId = postRes.body.fragment.id;
-  //   } else {
-  //     throw new Error('Fragment ID not found in response body');
-  //   }
+    logger.debug("Fragment has been posted: ", postRes);
 
-  //   const res = await request(app).get(`/v1/fragments/${fragmentId}`).auth('user1@email.com', 'password1');
+    const id = JSON.parse(postRes.text).fragment.id;
 
-  //   expect(res.statusCode).toBe(201);
-  //   expect(res.headers['content-type']).toBe('text/plain');
-  //   expect(res.text).toBe('This is a test fragment.');
-  // });
+    const getRes = await request(app).get('/v1/fragments/' + id).auth('user1@email.com', 'password1');
+
+    expect(getRes.statusCode).toBe(200);
+  });
 });
-
-
-// // tests/unit/getFragmentById.test.js
-
-// const request = require('supertest');
-// const app = require('../../src/app');
-
-// describe('GET /v1/fragments/:id/info', () => {
-
-//   test('Should return 404: fragment not found', async () => {
-//     const getRes = await request(app)
-//       .get('/v1/fragments/wrongId/info')
-//       .auth('user1@email.com', 'password1');
-
-//     console.log('getRes.body:', getRes.body); // Debug log
-
-//     expect(getRes.statusCode).toBe(404);
-//     expect(getRes.body).toStrictEqual({
-//       status: 'error',
-//       error: { code: 404, message: 'not found' }, // Updated message
-//     });
-//   });
-// });
-
-
 
